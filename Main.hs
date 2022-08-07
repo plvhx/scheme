2868bf76849f4b0e2b788a0a80495939c1b7ec30
plvhx <gandung@slackerc0de.us>
2022-08-07 18:18:40 +0700

    Refactoring Main.hs

--- Main.hs
+++ Main.hs
@@ -30,7 +30,7 @@ nullEnv = newIORef []
 type IOThrowsError = ExceptT LispError IO
 
 liftThrows :: ThrowsError a -> IOThrowsError a
-liftThrows (Left err) = throwError err
+liftThrows (Left err)  = throwError err
 liftThrows (Right val) = return val
 
 runIOThrows :: IOThrowsError String -> IO String
@@ -75,15 +75,10 @@ bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef 
         ref <- newIORef value
         return (var, ref)
 
---readExpr :: String -> ThrowsError LispVal
---readExpr input = case parse parseExpr "lisp" input of
---    Left err -> throwError $ Parser err
---    Right val -> return val
-
 spaces :: Parser ()
 spaces = skipMany1 space
 
-readExpr = readOrThrow parseExpr
+readExpr     = readOrThrow parseExpr
 readExprList = readOrThrow (endBy parseExpr spaces)
 
 data LispVal = Atom String
@@ -145,21 +140,21 @@ parseQuoted = do
     return $ List [Atom "quote", x]
 
 showVal :: LispVal -> String
-showVal (String contents) = "\"" ++ contents ++ "\""
-showVal (Atom name) = name
-showVal (Number contents) = show contents
-showVal (Bool True) = "#t"
-showVal (Bool False) = "#f"
-showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (String contents)      = "\"" ++ contents ++ "\""
+showVal (Atom name)            = name
+showVal (Number contents)      = show contents
+showVal (Bool True)            = "#t"
+showVal (Bool False)           = "#f"
+showVal (List contents)        = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
-showVal (PrimitiveFunc _) = "<primitive>"
+showVal (PrimitiveFunc _)      = "<primitive>"
 showVal (Func { params = args, vararg = varargs, body = body, closure = env }) =
     "(lambda (" ++ unwords (map show args) ++
         (case varargs of
             Nothing -> ""
             Just arg -> " . " ++ arg) ++ ") ...)"
-showVal (Port _) = "<IO Port>"
-showVal (IOFunc _) = "<IO Primitive>"
+showVal (Port _)               = "<IO Port>"
+showVal (IOFunc _)             = "<IO Primitive>"
 
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map showVal
@@ -167,10 +162,10 @@ unwordsList = unwords . map showVal
 instance Show LispVal where show = showVal
 
 eval :: Env -> LispVal -> IOThrowsError LispVal
-eval env val@(String _) = return val
-eval env val@(Number _) = return val
-eval env val@(Bool _) = return val
-eval env (Atom id) = getVar env id
+eval env val@(String _)             = return val
+eval env val@(Number _)             = return val
+eval env val@(Bool _)               = return val
+eval env (Atom id)                  = getVar env id
 eval env (List [Atom "quote", val]) = return val
 eval env (List [Atom "if", pred, conseq, alt]) =
     do
@@ -178,11 +173,11 @@ eval env (List [Atom "if", pred, conseq, alt]) =
         case result of 
             Bool False -> eval env alt
             otherwise -> eval env conseq
-eval env (List [Atom "set!", Atom var, form]) = eval env form >>= setVar env var
+eval env (List [Atom "set!", Atom var, form])   = eval env form >>= setVar env var
 eval env (List [Atom "define", Atom var, form]) = eval env form >>= defineVar env var
 eval env (List (Atom "define" : List (Atom var : params) : body)) =
     makeNormalFunc env params body >>= defineVar env var
-eval env (List [Atom "load", String filename]) = load filename >>= liftM last . mapM (eval env)
+eval env (List [Atom "load", String filename])  = load filename >>= liftM last . mapM (eval env)
 eval env (List (Atom "define" : DottedList (Atom var : params) varargs : body)) =
     makeVarArgs varargs env params body >>= defineVar env var
 eval env (List (Atom "lambda" : List params : body)) =
@@ -245,10 +240,6 @@ primitives = [("+", numericBinOp (+)),
               ("eqv?", eqv),
               ("equal?", equal)]
 
---primitiveBindings :: IO Env
---primitiveBindings = nullEnv >>= (flip bindVars $ map makePrimitiveFunc primitives) where
---    makePrimitiveFunc (var, func) = (var, PrimitiveFunc func)
-
 numericBinOp :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinOp op           []  = throwError $ NumArgs 2 []
 numericBinOp op singleVal@[_] = throwError $ NumArgs 2 singleVal
@@ -262,8 +253,8 @@ boolBinOp unpacker op args = if length args /= 2
                                 right <- unpacker $ args !! 1
                                 return $ Bool $ left `op` right
 
-numBoolBinOp = boolBinOp unpackNum
-strBoolBinOp = boolBinOp unpackStr
+numBoolBinOp  = boolBinOp unpackNum
+strBoolBinOp  = boolBinOp unpackStr
 boolBoolBinOp = boolBinOp unpackBool
 
 unpackNum :: LispVal -> ThrowsError Integer
@@ -278,22 +269,22 @@ unpackNum notNum     = throwError $ TypeMismatch "number" notNum
 unpackStr :: LispVal -> ThrowsError String
 unpackStr (String s) = return s
 unpackStr (Number s) = return $ show s
-unpackStr (Bool s) = return $ show s
-unpackStr notString = throwError $ TypeMismatch "string" notString
+unpackStr (Bool s)   = return $ show s
+unpackStr notString  = throwError $ TypeMismatch "string" notString
 
 unpackBool :: LispVal -> ThrowsError Bool
 unpackBool (Bool b) = return b
-unpackBool notBool = throwError $ TypeMismatch "boolean" notBool
+unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
 
 showError :: LispError -> String
-showError (UnboundVar message varname) = message ++ ": " ++ varname
+showError (UnboundVar message varname)  = message ++ ": " ++ varname
 showError (BadSpecialForm message form) = message ++ ": " ++ show form
-showError (NotFunction message func) = message ++ ": " ++ show func
-showError (NumArgs expected found) = "Expected: " ++ show expected
+showError (NotFunction message func)    = message ++ ": " ++ show func
+showError (NumArgs expected found)      = "Expected: " ++ show expected
     ++ " args; found values: " ++ unwordsList found
 showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
     ++ ", found " ++ show found
-showError (Parser parseErr) = "Parse error at " ++ show parseErr
+showError (Parser parseErr)             = "Parse error at " ++ show parseErr
 
 type ThrowsError = Either LispError
 
@@ -303,37 +294,37 @@ extractValue :: ThrowsError a -> a
 extractValue (Right val) = val
 
 car :: [LispVal] -> ThrowsError LispVal
-car [List (x : xs)] = return x
+car [List (x : xs)]         = return x
 car [DottedList (x : xs) _] = return x
-car [badArg] = throwError $ TypeMismatch "pair" badArg
-car badArgList = throwError $ NumArgs 1 badArgList
+car [badArg]                = throwError $ TypeMismatch "pair" badArg
+car badArgList              = throwError $ NumArgs 1 badArgList
 
 cdr :: [LispVal] -> ThrowsError LispVal
-cdr [List (x : xs)] = return $ List xs
-cdr [DottedList [_] x] = return x
+cdr [List (x : xs)]         = return $ List xs
+cdr [DottedList [_] x]      = return x
 cdr [DottedList (_ : xs) x] = return $ DottedList xs x
-cdr [badArg] = throwError $ TypeMismatch "pair" badArg
-cdr badArgList = throwError $ NumArgs 1 badArgList
+cdr [badArg]                = throwError $ TypeMismatch "pair" badArg
+cdr badArgList              = throwError $ NumArgs 1 badArgList
 
 cons :: [LispVal] -> ThrowsError LispVal
-cons [x1, List []] = return $ List [x1]
-cons [x, List xs] = return $ List $ x : xs
+cons [x1, List []]            = return $ List [x1]
+cons [x, List xs]             = return $ List $ x : xs
 cons [x, DottedList xs xlast] = return $ DottedList (x : xs) xlast
-cons [x1, x2] = return $ DottedList [x1] x2
-cons badArgList = throwError $ NumArgs 2 badArgList
+cons [x1, x2]                 = return $ DottedList [x1] x2
+cons badArgList               = throwError $ NumArgs 2 badArgList
 
 eqv :: [LispVal] -> ThrowsError LispVal
-eqv [(Bool arg1), (Bool arg2)] = return $ Bool $ arg1 == arg2
-eqv [(Number arg1), (Number arg2)] = return $ Bool $ arg1 == arg2
-eqv [(String arg1), (String arg2)] = return $ Bool $ arg1 == arg2
-eqv [(Atom arg1), (Atom arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Bool arg1), (Bool arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
 eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
-eqv [(List arg1), (List arg2)] = return $ Bool $ (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
+eqv [(List arg1), (List arg2)]             = return $ Bool $ (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
     where
         eqvPair (x1, x2) = case eqv [x1, x2] of
             Left err -> False
             Right (Bool val) -> val
-eqv [_,_] = return $ Bool False
+eqv [_,_]      = return $ Bool False
 eqv badArgList = throwError $ NumArgs 2 badArgList
 
 data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
@@ -351,7 +342,7 @@ equal [arg1, arg2] = do
         [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
     eqvEquals <- eqv [arg1, arg2]
     return $ Bool $ (primitiveEquals || let (Bool x) = eqvEquals in x)
-equal badArgList = throwError $ NumArgs 2 badArgList
+equal badArgList   = throwError $ NumArgs 2 badArgList
 
 flushStr :: String -> IO ()
 flushStr str = putStr str >> hFlush stdout
@@ -367,7 +358,7 @@ evalAndPrint env expr = evalString env expr >>= putStrLn
 
 makeFunc varargs env params body = return $ Func (map showVal params) varargs body env
 makeNormalFunc = makeFunc Nothing
-makeVarArgs = makeFunc . Just . showVal
+makeVarArgs    = makeFunc . Just . showVal
 
 ioPrimitives :: [(String, [LispVal] -> IOThrowsError LispVal)]
 ioPrimitives = [("apply", applyProc),
@@ -387,7 +378,7 @@ primitiveBindings = nullEnv >>= (flip bindVars $ map (makeFunc IOFunc) ioPrimiti
 
 applyProc :: [LispVal] -> IOThrowsError LispVal
 applyProc [func, List args] = apply func args
-applyProc (func : args) = apply func args
+applyProc (func : args)     = apply func args
 
 makePort :: IOMode -> [LispVal] -> IOThrowsError LispVal
 makePort mode [String filename] = liftM Port $ liftIO $ openFile filename mode
@@ -397,11 +388,11 @@ closePort [Port port] = liftIO $ hClose port >> (return $ Bool True)
 closePort _ = return $ Bool False
 
 readProc :: [LispVal] -> IOThrowsError LispVal
-readProc [] = readProc [Port stdin]
+readProc []          = readProc [Port stdin]
 readProc [Port port] = (liftIO $ hGetLine port) >>= liftThrows . readExpr
 
 writeProc :: [LispVal] -> IOThrowsError LispVal
-writeProc [obj] = writeProc [obj, Port stdout]
+writeProc [obj]            = writeProc [obj, Port stdout]
 writeProc [obj, Port port] = liftIO $ hPrint port obj >> (return $ Bool True)
 
 readContents :: [LispVal] -> IOThrowsError LispVal
